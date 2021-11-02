9a50d494e90b4dd89b36d476e27652d5df55027b
Meri <mgogi13@freeuni.edu.ge>
2021-11-02 12:27:08 +0400

    Add display and check order functions

--- src/index.js
+++ src/index.js
@@ -8,27 +8,52 @@ const todoLists = [
     id: 1,
   },
   {
-    description: 'Submit Project',
+    description: 'Submit the Project',
+    completed: false,
+    id: 3,
+  },
+  {
+    description: 'Walk the dog',
     completed: false,
     id: 2,
   },
 ];
 
+function checkOrder() {
+  todoLists.sort((a, b) => {
+    const index1 = a.id;
+    const index2 = b.id;
+
+    if (index1 < index2) {
+      return -1;
+    }
+    if (index1 > index2) {
+      return 1;
+    }
+    return 0;
+  });
+}
+
+function clearElement(element) {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+}
+
 function addTodo() {
+  clearElement(listContainer);
   todoLists.forEach((list) => {
     listContainer.innerHTML += `<li class="d-flex list-container">
-  <input type="checkbox" class="checkbox" checked>
-  <p class="items checked">${list.description}</p>
+  <input type="checkbox" class="checkbox">
+  <p class="items" id="${list.id}">${list.description}</p>
   <i class="fas fa-ellipsis-v dots"></i>
   </li>`;
   });
 }
 
-// function displayTodo() {
-//   a
-// }
+function displayTodo() {
+  checkOrder();
+  addTodo();
+}
 
-addTodo();
-listContainer.addEventListener('click', () => {
-  console.log();
-})
+displayTodo();
