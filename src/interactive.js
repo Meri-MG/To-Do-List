6c741d19a20fec43a85cbbec03965e5b5eea4c36
Meri <mgogi13@freeuni.edu.ge>
2021-11-03 16:49:42 +0400

    Update completed function

--- src/interactive.js
+++ src/interactive.js
@@ -7,23 +7,48 @@ export function saveToStorage(list) {
   return list;
 }
 
+// export function completed(element, list) {
+//   element.addEventListener('change', () => {
+//     const task = element.nextElementSibling.innerHTML;
+//     if (element.checked === true) {
+//       list.forEach((item) => {
+//         if (item.description === task) {
+//           item.completed = true;
+//         }
+//       });
+//       element.nextElementSibling.classList.add('linethrough');
+//     } else {
+//       list.forEach((item) => {
+//         if (item.description === task) {
+//           item.completed = false;
+//         }
+//         element.nextElementSibling.classList.remove('linethrough');
+//       });
+//     }
+//     saveToStorage(list);
+//   });
+// }
+
+function checkCompleted(list, task, element, bool) {
+  list.forEach((item) => {
+    if (item.description === task) {
+      item.completed = bool; // local
+    }
+    if (bool) {
+      element.nextElementSibling.classList.add('linethrough');
+    } else {
+      element.nextElementSibling.classList.remove('linethrough');
+    }
+  });
+}
+
 export function completed(element, list) {
   element.addEventListener('change', () => {
     const task = element.nextElementSibling.innerHTML;
     if (element.checked === true) {
-      list.forEach((item) => {
-        if (item.description === task) {
-          item.completed = true;
-        }
-      });
-      element.nextElementSibling.classList.add('linethrough');
+      checkCompleted(list, task, element, true);
     } else {
-      list.forEach((item) => {
-        if (item.description === task) {
-          item.completed = false;
-        }
-        element.nextElementSibling.classList.remove('linethrough');
-      });
+      checkCompleted(list, task, element, false);
     }
     saveToStorage(list);
   });
